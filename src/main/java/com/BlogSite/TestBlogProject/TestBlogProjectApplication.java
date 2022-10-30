34438a00f1628a702ef1ab6547f903943adb3705
Memesaurus <Greggrr1702@gmail.com>
2022-10-30 20:22:43 +0300

    init

---
+++ src/main/java/com/BlogSite/TestBlogProject/TestBlogProjectApplication.java
@@ -0,0 +1,13 @@
+package com.BlogSite.TestBlogProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBlogProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestBlogProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BlogSite/TestBlogProject/TestBlogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BlogSite.TestBlogProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBlogProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
